412a4dda564c67359f50c8e10bb832103c3d402c
Admin <Admin@DESKTOP-RDLMJLT>
2021-10-25 23:55:27 +0700

    Add project files.

---
+++ Bank-Events/BankAccount.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+
+namespace Bank_Events
+{
+    class BankAccount
+    {
+        public delegate void Operation(string info);
+        public event Operation AccEvent;
+        public int Money { get;private set;}
+
+        public BankAccount(int money)
+        {
+            Money = money;
+        }
+
+        public void BalansInfo()
+        {
+            Console.WriteLine($"На вашем счету {Money} рублей") ;
+            
+        }
+
+        public void PutMoney(int money)
+        {
+            Money += money;
+            Console.ForegroundColor = ConsoleColor.Green;
+            AccEvent?.Invoke($"* На счет поступило {money} рублей ");
+            Console.ResetColor();
+            BalansInfo();
+        }
+
+        public void TakeMoney(int money)
+        {
+            if (Money >= money)
+            {
+                Money -= money;
+                Console.ForegroundColor = ConsoleColor.Green;
+                AccEvent?.Invoke($"* Со счета снято {money} рублей ");
+                Console.ResetColor();
+                BalansInfo();
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                AccEvent?.Invoke("* На вашем счете недостаточно средств");
+                Console.ResetColor();
+                BalansInfo();
+            }
+
+        }
+    }
+}
---
+++ Bank-Events/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Bank_Events
+{
+    class Program
+    {
+        static void Main()
+        {
+            BankAccount account = new BankAccount(1000);
+            account.BalansInfo();
+            account.AccEvent += Info;
+            account.PutMoney(500);            
+            account.TakeMoney(300);           
+            account.TakeMoney(1500);
+            
+        }
+        public static void Info(string info)
+        {         
+            Console.WriteLine(info);            
+        }
+    
+    }
+}
